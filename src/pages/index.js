2e558ff58163576fdedeec62ec1689290d06397f
Julien Bras <julienbras@sidoine.org>
2023-01-20 22:20:14 -0500

    link error

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro"
+            to="/docs/quick_start"
           >
             How to use it - 2min ⏱️
           </Link>
