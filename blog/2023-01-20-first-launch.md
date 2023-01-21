610880ac544e24ed3521372612d12cf3e3a3d925
Julien Bras <julienbras@sidoine.org>
2023-01-20 22:05:44 -0500

    initial commit

---
+++ blog/2023-01-20-first-launch.md
@@ -0,0 +1,12 @@
+---
+slug: first-launch
+title: First Launch
+authors:
+  name: Julien Bras
+  # title: Docusaurus Core Team
+  url: https://github.com/julbrs
+  image_url: https://github.com/julbrs.png
+tags: [testsendr]
+---
+
+We are happy to launch the first version of **TestSendr**, the tool that will help your **QA team** to build efficient **email 📬 testing scenarios**! Don't forget to get a look at our [Quick Start](/docs/quick_start) page to understand how to use the service.
---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,66 @@
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
+
+const FeatureList = [
+  {
+    title: "Test your Emails",
+    Svg: require("@site/static/img/undraw_mailbox_re_dvds.svg").default,
+    description: (
+      <>
+        TestSendr is a useful service if you need to check in your tests that
+        emails are correclty send with the right content. Ideal for your QA
+        team.
+      </>
+    ),
+  },
+  {
+    title: "API First",
+    Svg: require("@site/static/img/undraw_connected_world_wuay.svg").default,
+    description: (
+      <>
+        TestSendr is providing an easy-to-use single point API to retrieve
+        programmatically the content of the emails sent to{" "}
+        <strong>@testsendr.link</strong> address.
+      </>
+    ),
+  },
+  {
+    title: "Attachments supported",
+    Svg: require("@site/static/img/undraw_folder_files_re_2cbm.svg").default,
+    description: (
+      <>
+        TestSendr is supporting attachments in emails. See the number, name,
+        disposition of each attachments.
+      </>
+    ),
+  },
+];
+
+function Feature({ Svg, title, description }) {
+  return (
+    <div className={clsx("col col--4")}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
